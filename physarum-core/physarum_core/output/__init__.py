00bdde29e60172c49fee31721701507362e74c60
Moishe Lettvin <moishel@gmail.com>
2025-06-28 16:25:09 -0600

    Re-add output module

---
+++ physarum-core/physarum_core/output/__init__.py
@@ -0,0 +1,6 @@
+# ABOUTME: Output module initialization for physarum_core package
+# ABOUTME: Exports OutputManager class for use by other modules
+
+from .manager import OutputManager
+
+__all__ = ['OutputManager']
